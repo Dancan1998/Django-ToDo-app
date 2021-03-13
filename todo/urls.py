20becaa61d51ade056beb5a23ebc1e29bb4f5bc5
Dancan Chibole <dancankingstar@gmail.com>
2021-03-12 20:04:17 -0800

    listview & detailview

--- todo/urls.py
+++ todo/urls.py
@@ -1,3 +1,8 @@
-# from django.urls import path
+from django.urls import path
+from .views import TaskListView, TastDetailView
+app_name = 'todo'
 
-# urlpatterns=[]
+urlpatterns = [
+    path('', TaskListView.as_view(), name='task-list'),
+    path('task/<int:pk>', TastDetailView.as_view(), name='task-detail'),
+]
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Task
 
-# Create your views here.
+
+class TaskListView(ListView):
+    model = Task
+    context_object_name = 'tasks'
+
+
+class TastDetailView(DetailView):
+    model = Task
+    context_object_name = 'task_detail'
